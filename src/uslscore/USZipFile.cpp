13c1d538d73a6e44e86ee7c0c374a7007f596768
jpoka <joe@elevateapp.com>
2023-03-03 14:12:36 -0800

    Smaller footprint change in USZipEntry

--- src/uslscore/USZipFile.cpp
+++ src/uslscore/USZipFile.cpp
@@ -49,6 +49,14 @@ bool USZipEntry::SerializeIn ( USStream& stream ) {
 	stream.Seek ( this->mCompressedSize, SEEK_CUR );
 	
 	if ( this->mBitFlag & BIT_HAS_DESCRIPTOR ) {
+
+		u32 sigOrCRC = stream.Read < u32 >( 0 );
+
+		//crc-32 											4 bytes
+		if ( sigOrCRC == 0x04034b50 ) {
+			stream.Read < u32 >( 0 );
+		}
+
 		stream.Read < u32 >( 0 ); //compressed size			4 bytes
 		stream.Read < u32 >( 0 ); //uncompressed size		4 bytes
 	}
