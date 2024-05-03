cdaae8f00996051a2c95b258bfe83b74d34bf306
Aaron Barrett <aaron@aaronbarrett.com>
2024-05-03 12:29:05 -0700

    Removed sources for USHashWriter and subclasses.

--- src/uslscore/uslscore.h
+++ src/uslscore/uslscore.h
@@ -36,16 +36,6 @@
 #include <uslscore/USFileSys.h>
 #include <uslscore/USFloat.h>
 #include <uslscore/USFrustum.h>
-#include <uslscore/USHashWriter.h>
-#include <uslscore/USHashWriterAdler32.h>
-#include <uslscore/USHashWriterCRC32.h>
-#include <uslscore/USHashWriterMD5.h>
-#include <uslscore/USHashWriterSHA1.h>
-#include <uslscore/USHashWriterSHA224.h>
-#include <uslscore/USHashWriterSHA256.h>
-#include <uslscore/USHashWriterSHA384.h>
-#include <uslscore/USHashWriterSHA512.h>
-#include <uslscore/USHashWriterWhirlpool.h>
 #include <uslscore/USHexDump.h>
 #include <uslscore/USHexReader.h>
 #include <uslscore/USHexWriter.h>
